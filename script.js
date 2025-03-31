6504503f66a389288f4973349b8ec5eb59aa627c
SiddhiMaru26 <siddhimaru032@gmail.com>
2025-03-31 12:39:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var typed = new Typed(".text", { 
+    strings: ["Backend Developer", "Software Engineer", "Web Developer"], 
+    typeSpeed: 100,  // Speed of typing
+    backSpeed: 100,  // Speed of deleting
+    backDelay: 1000, // Delay before deleting
+    loop: true       // Keep looping
+});
+
+
